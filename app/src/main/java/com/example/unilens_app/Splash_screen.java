2da424a278234751b7b31c6849c323965655a18a
H.W.S.M.MUNASINGHE <2022t01534@stu.cmb.ac.lk>
2025-06-03 14:33:56 +0530

    Initial development of the splash screen page

--- app/src/main/java/com/example/unilens_app/Splash_screen.java
+++ app/src/main/java/com/example/unilens_app/Splash_screen.java
@@ -1,6 +1,8 @@
 package com.example.unilens_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +10,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class Splash_screen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splash_screen.this, MainActivity.class));
+                finish();
+            }
+        }, 2000);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
